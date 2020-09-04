97e0532bdb71dc7f91d4c2cebb0a05e0c8d9366f
chenweiyan <chanweiyan007@gmail.com>
2020-09-04 19:23:42 +0800

    第一屏动画

--- 1.前端零基础入门/宣传页项目作业5-6/js/index.js
+++ 1.前端零基础入门/宣传页项目作业5-6/js/index.js
@@ -18,6 +18,30 @@ function setCls(elem, cls) {
   return elem.setAttribute('class', cls);
 }
 
+// 给元素添加类
+function addCls(elem, cls) {
+  var baseCls = getCls(elem);
+  if (baseCls.indexOf(cls) != -1) return;
+  setCls(elem, baseCls + ' ' + cls);
+}
+
+// 删除元素的类
+function delCls(elem, cls) {
+  setCls(elem, getCls(elem).split(cls).join(' ').replace(/\s+/g, ' '));
+}
+
+// 需要设置动画的元素
+var screenAnimateElements = {
+  '.screen-1': [
+    '.screen-1__heading',
+    '.screen-1__desc'
+  ],
+  '.screen-2': [],
+  '.screen-3': [],
+  '.screen-4': [],
+  '.screen-5': []
+}
+
 // 当鼠标切换导航项时，下划线跟随鼠标移动到相应的导航项上
 var navItems = getAllElem('.header__nav-item');
 var navTip = getElem('.header__nav-tip');
@@ -42,3 +66,31 @@ function setNavTip(idx, items) {
 for (var i = 0; i < navItems.length; i++) {
   setNavTip(i, navItems);
 }
+
+// 初始化元素的状态
+function setAnimateInit(screen) {
+  var animateElements = screenAnimateElements[screen];
+  for (var i = 0; i < animateElements.length; i++) {
+    var element = getElem(animateElements[i])
+    addCls(element, animateElements[i].substr(1) + '_animate_init');
+  }
+}
+
+// 播放动画 init --> done
+function playAnimateDone(screen) {
+  var animateElements = screenAnimateElements[screen];
+  for (var i = 0; i < animateElements.length; i++) {
+    var element = getElem(animateElements[i])
+    setCls(element, getCls(element).replace('_animate_init', '_animate_done'));
+  }
+}
+
+window.addEventListener('load', function () {
+  for (var k in screenAnimateElements) {
+    setAnimateInit(k);
+  }
+
+  // 页面加载完成，1秒后自动播放第一屏动画
+  setTimeout(function () { playAnimateDone('.screen-1') }, 1000);
+});
+
