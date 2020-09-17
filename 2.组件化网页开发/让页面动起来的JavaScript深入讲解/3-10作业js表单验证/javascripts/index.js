a5ac955c8f5a08c8468eb44f9c3a61a63322e52c
chenweiyan <chanweiyan007@gmail.com>
2020-09-18 04:46:44 +0800

    姓名填写规则

--- 2.组件化网页开发/让页面动起来的JavaScript深入讲解/3-10作业js表单验证/javascripts/index.js
+++ 2.组件化网页开发/让页面动起来的JavaScript深入讲解/3-10作业js表单验证/javascripts/index.js
@@ -33,10 +33,10 @@ dropdownContent.addEventListener('mouseout', function () {
 })
 
 // 主体
-var tips = getElems('.tips, .password_tips');
 var inputs = {
   username: byId('username'),
-  password: byId('password')
+  password: byId('password'),
+  'nameZhCn': byId('nameZhCn'),
 }
 var patterns = {
   username: /^[a-zA-Z]\w{5,29}$/, //6-30位字母、数字或'_'，字母开头
@@ -44,15 +44,18 @@ var patterns = {
   p_low: /^\d+$|^[a-zA-Z]+$|^[^\s\da-zA-Z]+$/, //内容是纯数字、纯字母或者纯符号，密码强度为低
   p_normal: /^[^a-zA-Z]+$|^[^\d]+$|^[\da-zA-Z]+$/, //是两种类型的结合，密码强度为一般
   p_high: /\d+[a-zA-Z]+[^\s\da-zA-Z]|[a-zA-Z]+[^\s\da-zA-Z]+\d|[^\s\da-zA-Z]\d+[a-zA-Z]/, //是三种类型的结合，那么密码强度为高
+  'nameZhCn': /^[\u4e00-\u9fa5a-zA-Z]{3,30}$/i, //姓名只能包含中文或者英文,且中文字符在2-15之间，英文字符在3-30个之间。
 }
 var checkResults = {};
 var errMsgs = {
   username: "6-30位字母、数字或'_'，字母开头",
   password: '6-20位字母、数字或符号',
+  'nameZhCn': '姓名只能包含中文或者英文,且字符在3-30个之间！',
 }
 var successMsgs = {
   username: '用户名输入正确',
   password: '',
+  'nameZhCn': '姓名输入正确',
 }
 
 // 检查密码的强度
@@ -77,6 +80,31 @@ function checkPasswordLevel() {
 }
 inputs['password'].addEventListener('blur', checkPasswordLevel);
 
+// 姓名填写规则
+getElem('.name-rule').addEventListener('mouseover', function() {
+  getElem('.name-rule-content').style.display = 'block';
+})
+getElem('.name-rule').addEventListener('mouseout', function () {
+  getElem('.name-rule-content').style.display = 'none';
+})
+
+// 密码确认框
+function checkPasswordConfirm() {
+  var tips = getInputTipsOf('password_confirm');
+  if (this.value == '') {
+    tips.innerHTML = '输入框不能为空';
+    tips.style.color = 'red';
+  } else if (this.value != inputs['password'].value) {
+    tips.innerHTML = '两次密码输入不一致，请重新输入';
+    tips.style.color = 'red';
+  } else {
+    tips.innerHTML = '两次输入一致';
+    tips.style.color = 'green';
+  }
+}
+byId('password_confirm').addEventListener('blur', checkPasswordConfirm);
+
+// 用户名、登录密码
 function checkFormatOf(selector) {
   inputs[selector].addEventListener('blur', function() {
     var tips = getInputTipsOf(selector);
