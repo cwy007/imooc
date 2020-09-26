744c63e3765d29c2bbd1418cdd713e185cc47d82
chenweiyan <chanweiyan007@gmail.com>
2020-09-26 19:18:11 +0800

    ui-tab

--- 2.组件化网页开发/步骤二：一个快速、简洁的JS库--jQuery/5-12/5-12作业-医院详情/js/ui.js
+++ 2.组件化网页开发/步骤二：一个快速、简洁的JS库--jQuery/5-12/5-12作业-医院详情/js/ui.js
@@ -40,19 +40,17 @@ $.fn.UiBackTop = function () {
  * ui-tab
  * @param {string} header  TAB组件，的所有选项卡 item
  * @param {string} content TAB组件，内容区域，所有 item
- * @param {string} focus_prefix  选项卡高亮样式前缀，可选
  */
-$.fn.UiTab = function (header, content, focus_prefix) {
+$.fn.UiTab = function (header, content) {
   var ui = $(this),
     tabs = $(header, ui),
-    cons = $(content, ui),
-    focus_prefix = focus_prefix || '';
+    cons = $(content, ui);
 
   tabs.on('click', function () {
     var index = $(this).index();
-    tabs.removeClass(focus_prefix + 'item_focus')
+    tabs.removeClass('active')
       .eq(index)
-      .addClass(focus_prefix + 'item_focus');
+      .addClass('active');
     cons.hide().eq(index).show();
     return false;
   });
@@ -62,4 +60,5 @@ $.fn.UiTab = function (header, content, focus_prefix) {
 $(function () {
   $('.ui-search').UiSearch();
   $('body').UiBackTop();
+  $('.content-tab').UiTab('.content-tab-captions-item', '.content-tab-details-item');
 });
